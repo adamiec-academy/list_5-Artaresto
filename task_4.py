f3c6fb70def59bfe28a01eb06fc19ea04c42765d
Artaresto <89027893+Artaresto@users.noreply.github.com>
2022-12-30 18:42:51 +0100

    Update task_4.py

--- task_4.py
+++ task_4.py
@@ -1,20 +1,20 @@
+
 def reversed_words():
     data = []
 
-    for unit in open("words.txt", encoding="utf-8"):
+    for unit in open("listy/lista05/words.txt", encoding="utf-8"):
         data.append(unit.strip())
     
     unique_words = set(data)
-    words_repeated = set()
+    backwards_check = {word[::-1] for word in unique_words if word != word[::-1]}
     results = []
+    check_set = set()
     
     for word in unique_words:
-        reversed_word = word[::-1]
-        if reversed_word in words_repeated:
+        if word in words_repeated and word not in check_set:
+            reversed_word = word[::-1]
             results.append((word, reversed_word))
-        else:
-            words_repeated.add(word)
-
- 
+            check_set.add(word)
+            check_set.add(reversed_word)
 
     return results
