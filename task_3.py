c7249d11dce7ff08bbbe11305f2e1f76969faacc
Artaresto <89027893+Artaresto@users.noreply.github.com>
2022-12-30 14:34:46 +0100

    Update task_3.py

--- task_3.py
+++ task_3.py
@@ -1,1 +1,18 @@
-# Remove this comment to confirm that this task is done
+from turtle import * 
+data = []
+
+reverse_pos_x = -410
+reverse_pos_y = 320
+
+for unit in open("listy/lista05/dots.txt"):
+    data.append(unit.strip().split(" "))
+  
+pu()
+goto(int(data[0][0])  + reverse_pos_x, -int(data[0][1]) + reverse_pos_y)
+pd()
+    
+for i in range(len(data)):
+    x, y = int(data[i][0]) + reverse_pos_x, -int(data[i][1]) + reverse_pos_y
+    goto(x, y) 
+update()
+exitonclick()
