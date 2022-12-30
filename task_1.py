10d1bcd9bb3fb6368b514302ff5e32ccc240567e
Artaresto <89027893+Artaresto@users.noreply.github.com>
2022-12-30 12:12:29 +0100

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,14 +1,11 @@
-def longest_word(file):
+def longest_word():
     data = []
-    
+
     for unit in open("words.txt", encoding="utf-8"):
         data.append(unit.strip())
 
-    longest_word = ""
+    the_longest_word = ""
     for word in data:
-        if len(word) > len(longest_word):
-            longest_word = word
-    return longest_word
-
-longest_word("words.txt")
-    
+        if len(word) > len(the_longest_word):
+            the_longest_word = word
+    return the_longest_word
