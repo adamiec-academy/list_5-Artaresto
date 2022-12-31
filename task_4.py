c9010eec78bcfb26eeebe4a525aa94e76d77c28e
Artaresto <89027893+Artaresto@users.noreply.github.com>
2022-12-31 15:55:46 +0100

    Update task_4.py

--- task_4.py
+++ task_4.py
@@ -4,15 +4,13 @@ def reversed_words():
     for unit in open("words.txt", encoding="utf-8"):
         data.add(unit.strip())
     
-
     results = []
     
     for word in data:
         reversed_word = word[::-1]
         if reversed_word in data and  reversed_word != word and word not in results:
             results.append(tuple(sorted((word, reversed_word))))
-    
-    results = list(set(results))        
+          
     results.sort()
     return results
 
